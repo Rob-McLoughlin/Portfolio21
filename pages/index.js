34e545ae0770b61e81e648297a133acc081d38e2
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-08-28 22:50:37 +0100

    feat(content): updated content for new role

--- pages/index.js
+++ pages/index.js
@@ -24,21 +24,26 @@ export default function Home ({ books }) {
       </Head>
       <div className='flex justify-center px-8 bg-white dark dark:bg-black'>
         <main className='relative container mx-auto max-w-2xl lg:max-w-xl py-48'>
-          <div className='absolute rounded-full top-12 right-6 border-2 border-dashed border-black dark:border-white z-0 h-16 w-16' />
+          <div className='absolute rounded-full top-12 right-6 border-2 border-dashed border-black dark:border-white z-0 h-20 w-20' />
           <div className='absolute top-12 right-12 z-1'>
             <Image
               className='rounded-full'
               alt='A profile image of me in black and white'
-              src='/rob-linkedin.jpeg'
-              height={64}
-              width={64}
+              src='/rob_400.jpg'
+              height={80}
+              width={80}
             />
           </div>
 
           <section id='#top' className='relative mb-8'>
-            <h1>Hi, I’m Rob McLoughlin</h1>
+            <h1>Rob McLoughlin</h1>
             <p>
               I’m a product designer building digital products at{' '}
+              <Link href='https://www.brownthomas.com/'>
+                <a>Brown Thomas Arnotts</a>
+              </Link>
+              . I work across the product life-cycle from strategy and research
+              through design and development. Previously, I worked at{' '}
               <Link href='https://www.wearehuman.ie/'>
                 <a>Human</a>
               </Link>{' '}
@@ -46,13 +51,10 @@ export default function Home ({ books }) {
               <Link href='https://www.chupi.com/'>
                 <a>Chupi</a>
               </Link>
-              . I work across the product life-cycle from strategy and research
-              through design and development.
+              .
             </p>
-            <p>
-              I am not very active online, but you can contact me by email,
-              Instagram, Twitter or LinkedIn.
-            </p>
+            <p></p>
+            <p>You can contact me by Email, Instagram, Twitter or LinkedIn.</p>
           </section>
 
           <SocialLinks socials={socials} />
@@ -60,9 +62,8 @@ export default function Home ({ books }) {
           <section id='technologies'>
             <h1>Technologies</h1>
             <p>
-              I have been actively learning about a lot of different
-              technologies in order to build great user experiences. I take
-              pride in my ability to deliver designs in high-quality codebases.
+              I am an experienced front-end developer. I take pride in my
+              ability to deliver designs in high-quality codebases.
             </p>
             <p>
               Here are some technologies I’ve been making use of in the last six
@@ -83,14 +84,13 @@ export default function Home ({ books }) {
               <Link href='https://www.wearehuman.ie/'>
                 <a>Human</a>
               </Link>
-              . When I finished my undergrad, I went back and have been there
-              since! During my time at Human, the majority of my work has been
-              on{' '}
+              . When I finished my undergrad, I went back to Human. During my
+              time at Human, the majority of my work was with{' '}
               <Link href='https://www.chupi.com/'>
                 <a>Chupi</a>
               </Link>
               , a global luxury jewellery company based in Ireland. In 2021, I
-              became the User Experience Manager at Chupi. My work there has
+              became the User Experience Manager at Chupi. My work there
               included things like{' '}
               {Object.entries(projects).map((p, index) => {
                 return (
@@ -109,12 +109,18 @@ export default function Home ({ books }) {
               .
             </p>
             <p>
-              I like to read about design, I go through phases of chomping
-              through books about design and business.
+              In 2022, I joined{' '}
+              <Link href='https://www.brownthomas.com/'>
+                <a>Brown Thomas Arnotts</a>
+              </Link>{' '}
+              as a UX/UI Designer. There, I work on the digital experience
+              across their online stores.
             </p>
-            <h2>Recently Read Books</h2>
+            <h2 className='mt-10'>Recently Read Books</h2>
             <p>
-              Pulled from my personal Notion using{' '}
+              I like to read about design, I go through phases of chomping
+              through books about design and business. These recently read books
+              are pulled from my personal Notion using{' '}
               <a
                 href='https://developers.notion.com/'
                 target='_blank'
